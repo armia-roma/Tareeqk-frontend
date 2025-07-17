fb955773234a6f8844fd033b00ed4933c9006d9e
armia-roma <armia_bos@gmail.com>
2025-07-17 12:20:52 +0400

    feat: add FormInput component for reusable input fields with validation support

---
+++ src/Components/Ui/FormInput.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface FormInputProps {
+	name: string;
+	type?: string;
+	icon?: React.ReactNode;
+	placeholder?: string;
+	register: any; // or use: UseFormRegister<any>
+	error?: string;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+	name,
+	type = "text",
+	icon,
+	placeholder,
+	register,
+	error,
+}) => {
+	return (
+		<div className="space-y-1">
+			<div className="flex items-center border rounded-xl pl-3 pr-3">
+				{icon}
+				<input
+					type={type}
+					{...register(name)}
+					className="w-full py-3 px-3 outline-none"
+					placeholder={placeholder}
+				/>
+			</div>
+			{error && (
+				<p className="text-red-500 text-start text-sm">{error}</p>
+			)}
+		</div>
+	);
+};
+
+export default FormInput;
