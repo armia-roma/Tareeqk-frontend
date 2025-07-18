5bf6e583a1281700bafdde6238889e6175343a23
armia-roma <armia_bos@gmail.com>
2025-07-18 15:45:10 +0400

    refactor: improve TowingRequestPage structure and handle location validation

--- src/Pages/TowingRequestPage.tsx
+++ src/Pages/TowingRequestPage.tsx
@@ -1,32 +1,46 @@
 import Map from "./../Components/TowingRequest/Map";
 import Header from "../Components/TowingRequest/Header";
 import TowingForm from "../Components/TowingRequest/TowingForm";
-import {useTowingRequest} from "../hooks/useTowingRequest";
+import { useTowingRequest } from "../hooks/useTowingRequest";
+import TowingRequestOverlay from "../Components/TowingRequestOverlay";
+import { useState } from "react";
+import useCancelTowingRequest from "../hooks/useCancelTowingRequest";
+import Loading from "../Components/Ui/Loading";
+interface ITowing {
+	id: number;
+}
 const TowingRequestPage = () => {
-	const {location, setLocation, submitRequest, error, loading} =
-		useTowingRequest();
-
-	const handleFormSubmit = async (data: {note: string}) => {
-		if (!location) {
-			alert("Please select a location on the map.");
-			return;
-		}
-		const result = await submitRequest(data.note);
-		if (result) {
-			console.log("Towing request submitted:", result);
+	const { setLocation, submitRequest, error, loading } = useTowingRequest();
+	const [showOverlay, setShowOverlay] = useState(false);
+	const [towing, setTowing] = useState<ITowing>({} as ITowing);
+	const { cancelTowing } = useCancelTowingRequest();
+	const handleFormSubmit = async ({ note }: { note: string }) => {
+		const result = await submitRequest(note);
+		if (result.success) {
+			setShowOverlay(true);
+			setTowing(result.data);
 		}
 	};
 	const handleLocationChange = (location: google.maps.LatLngLiteral) => {
-		setLocation(location); // Store in state or pass to form
+		setLocation(location);
 	};
+	const handleOnCancel = async () => {
+		await cancelTowing(towing.id);
+		setShowOverlay(false);
+	};
+	if (loading) {
+		<Loading />;
+	}
+
 	return (
-		<div className="max-h-screen max-w-[450px] mx-auto">
-			<div className="flex flex-col space-y-3">
+		<div className="max-h-screen max-w-[450px] mx-auto relative">
+			<div className={`flex flex-col space-y-3 `}>
 				<Header />
 				<h2 className="text-2xl font-semibold">Request a Tawing </h2>
 				<Map onLocationChange={handleLocationChange} />
 				<TowingForm onSubmit={handleFormSubmit} />
 			</div>
+			{showOverlay && <TowingRequestOverlay onCancel={handleOnCancel} />}
 		</div>
 	);
 };
